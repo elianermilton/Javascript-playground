ad2bdedc91cda99ee0a78a7b26c4383471cc6728
elianermilton <elianermilton@gmail.com>
2025-11-14 14:38:26 -0500

    Update myfirst.js

--- myfirst.js
+++ myfirst.js
@@ -1,1 +1,14 @@
-console.log("Hello Éliane. This script is live!");
+const text = "Welcome to my JavaScript Playground.";
+let index = 0;
+
+function typeIntro() {
+    const introElement = document.getElementById("intro");
+    introElement.textContent = text.slice(0, index);
+    index++;
+
+    if (index <= text.length) {
+        setTimeout(typeIntro, 80);
+    }
+}
+
+typeIntro();
