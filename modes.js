b31ca84107fa0f7b83fab62a8951869aa14d62eb
elianermilton <elianermilton@gmail.com>
2025-11-15 11:28:11 -0500

    Add event listeners for mode buttons

---
+++ modes.js
@@ -0,0 +1,9 @@
+const title = document.getElementById("title");
+const subtitle = document.getElementById("subtitle");
+const body = document.body;
+
+const softButton = document.getElementById("soft-mode");
+const focusButton = document.getElementById("focus-mode");
+
+softButton.addEventListener("click", ...);
+focusButton.addEventListener("click", ...);
